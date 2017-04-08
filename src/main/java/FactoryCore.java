bd84b298ce5e7391ac73a5a58393ea4708e42902
ALEXEY RYKHALSKIY <alexey.rykhalskiy@gmail.com>
2017-04-08 23:07:54 +0300

    2016-04-08 migration to Raspberry in process 4

--- src/main/java/FactoryCore.java
+++ src/main/java/FactoryCore.java
@@ -4,14 +4,9 @@ import tests.Test_DOS_31;
 public class FactoryCore {
     public static void main(String[] args) throws Exception {
         if (args.length < 1 ) {
-            System.out.println(String.format("Parameter %1 should be COM Port "));
+            new Test_COMPortList().run();
         } else {
-            if ("list".equals(args[0])) {
-                new Test_COMPortList().run();
-            }
-            else {
-                new Test_DOS_31().run(args[0]);
-            }
+            new Test_DOS_31().run(args[0]);
         }
     }
 }
