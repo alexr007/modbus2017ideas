eb5a4c5cb3a7e9500b769d6ddf3c98dc3a6d999c
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2017-08-27 22:55:00 +0300

    minor changes

--- src/main/java/j2bus/comport/COMPortList.java
+++ src/main/java/j2bus/comport/COMPortList.java
@@ -2,6 +2,7 @@ package j2bus.comport;
 
 import com.google.common.base.Joiner;
 import com.jcabi.aspects.Timeable;
+import j1base.IterableToString;
 import jssc.SerialPortList;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Created by alexr on 02.12.2016.
@@ -17,35 +19,19 @@ public class COMPortList {
     private final String PREFIX = ":[";
     private final String POSTFIX = "]";
     private final String DELIM = ", ";
-    private boolean ready = false;
-    private final Collection<CharSequence> list = new ArrayList<>();
 
+    //@Timeable(limit = 500, unit = TimeUnit.MILLISECONDS)
     public Collection<CharSequence> get()
     {
-        doRead();
-        return list;
-    }
-
-    public int count()
-    {
-        doRead();
-        return list.size();
+        return Arrays.stream(SerialPortList.getPortNames(
+            "/dev/",
+            //"tty.(serial|usbserial|usbmodem).*"
+            Pattern.compile("(tty.*UART*|tty.*serial*)")
+            ))
+            .collect(Collectors.toList());
     }
 
     public String toString() {
-        return Joiner.on("").join(count(),PREFIX,Joiner.on(DELIM).join(get()),POSTFIX);
+        return new IterableToString<>(get()).toString();
     }
-
-    //@Timeable(limit = 500, unit = TimeUnit.MILLISECONDS)
-    private void doRead() {
-        if (!ready) {
-            list.addAll(Arrays.asList(SerialPortList.getPortNames(
-                    "/dev/",
-                    //"tty.(serial|usbserial|usbmodem).*"
-                    Pattern.compile("(tty.*UART*|tty.*serial*)")
-            )));
-            ready = true;
-        }
-    }
-
 }
